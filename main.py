c7710eb86a492a8cc9d1b32b40a967f571ba6ded
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2023-02-16 17:23:53 -0500

    Added Myles as Student

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def student_init():
     stud3 = Student(10615734,"Nick","Rose", 22, list_of_majors[1])
     stud4 = Student(10633211, "Dorian","Freeman", 26, list_of_majors[3])
     stud5 = Student(10655321,"Mary","Jones", 21, list_of_majors[4])
+    stud6 = Student(10669520, "Myles", "Davie", 20, list_of_majors[0])
     list_of_active_students.extend([stud1,stud2,stud3,stud4,stud5])
 
 
